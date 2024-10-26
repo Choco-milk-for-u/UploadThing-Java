ace3e5e15530a0952b0cff3a2d71486365f34ca8
Choco-milk-for-u <84465375+Choco-milk-for-u@users.noreply.github.com>
2024-10-27 00:58:46 +0400

    feat: add utapi config

---
+++ uploadthing-java/src/main/java/org/chocodev/UTApiConfig.java
@@ -0,0 +1,9 @@
+package org.chocodev;
+
+
+public class UTApiConfig {
+    public final static String requestError = "";
+    public final static String UTFileName = "filename";
+    public final static String accessFileUrl = "https://utfs.io";
+    public final static String apiFieldError = "";
+}
