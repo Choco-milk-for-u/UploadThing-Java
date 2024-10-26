55dda06d15f7bf356508f16a0939593895a4deaf
Choco-milk-for-u <84465375+Choco-milk-for-u@users.noreply.github.com>
2024-10-27 01:12:53 +0400

    chore: add error message when file is not recived

--- uploadthing-java/src/main/java/org/chocodev/UTApiConfig.java
+++ uploadthing-java/src/main/java/org/chocodev/UTApiConfig.java
@@ -3,6 +3,7 @@ package org.chocodev;
 
 public class UTApiConfig {
     public final static String requestError = "";
+    public final static String fileReciveError = "";
     public final static String UTFileName = "filename";
     public final static String accessFileUrl = "https://utfs.io";
     public final static String apiFieldError = "";
