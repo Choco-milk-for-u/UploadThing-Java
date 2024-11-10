301bd2caf2b41e022c1618cac9ae457e1b3a9999
Choco-milk-for-u <84465375+Choco-milk-for-u@users.noreply.github.com>
2024-11-10 06:28:52 +0400

    fix: wrong naming of constants

--- src/main/java/org/chocodev/util/Constants/ContentDisposition.java
+++ src/main/java/org/chocodev/util/Constants/ContentDisposition.java
@@ -1,15 +1,28 @@
 package org.chocodev.util.Constants;
 
+/**
+ * ContentDisposition Enum.
+ * For more information, see the {@link https://docs.uploadthing.com/api-reference/openapi-spec}.
+ */
 public enum ContentDisposition {
-    PUBLIC("attachment"),
-    PRIVATE("inline");
+    /**
+    * @return attachment
+    */
+    ATTACHMENT("attachment"),
+    /**
+    * @return inline
+    */
+    INLINE("inline");
 
     private final String value;
 
     ContentDisposition(String value) {
         this.value = value;
     }
-
+    /**
+     * get value from constants.
+     * @return string value.
+     */
     public String getValue() {
         return value;
     }
